824c4abc7f1e617774cf06100d8d01c419c0fd09
Hamza_Sheikh <hamza.sheikh888777@gmail.com>
2024-05-17 11:32:07 +0500

    👉 Project (Number Guessing Game) 😃

---
+++ Project01_Number_Guessing_Game/index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Guess a number between 1-5:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("You guessed right");
+}
+else {
+    console.log("You guessed wrong");
+}
